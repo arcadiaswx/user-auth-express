1d93fd5fdf3d1804b24a669edca7bdcd115b271d
Craig Booker <info@itech.plus>
2018-07-16 21:16:44 -0500

    Added a route to delete the session when the user clicks on logout.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,20 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user");
 
+// GET /logout
+router.get("/logout", function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect("/");
+      }
+    });
+  }
+});
+
 // Get /profile
 router.get("/profile", function(req, res, next) {
   if (!req.session.userId) {
