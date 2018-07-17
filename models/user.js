0b91457da4b3a6579b172a91bf3c89ff820f1e26
Craig Booker <info@itech.plus>
2018-07-16 20:09:43 -0500

    Added the authenticate function which allows validating user input against the database.

--- models/user.js
+++ models/user.js
@@ -22,6 +22,27 @@ var UserSchema = new mongoose.Schema({
     required: true
   }
 });
+// Authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email }).exec(function(error, user) {
+    if (error) {
+      return callback(error);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    // Bcrypt compare takes the password the user entered, the password from the // db, and presents an error if something goes wrong, or returns the results.
+    bcrypt.compare(password, user.password, function(error, result) {
+      if (result == true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
+
 // Hash password before saving to database
 // SAVE is known by Mongo and then we have an annonymous function.
 UserSchema.pre("save", function(next) {
